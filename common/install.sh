edfe5bb4e97201a02a56ba62b7b4276e66fc78c8
hiddify <hiddify@gmail.com>
2023-02-27 16:41:14 +0100

    add menu

--- common/install.sh
+++ common/install.sh
@@ -1,7 +1,7 @@
 sudo timedatectl set-timezone  Asia/Tehran
 apt update
-apt install -y at apt-transport-https dnsutils ca-certificates git curl wget gnupg-agent software-properties-common  iptables locales lsof cron
-sudo apt -y remove needrestart apache2
+apt install -y at dialog apt-transport-https dnsutils ca-certificates git curl wget gnupg-agent software-properties-common  iptables locales lsof cron
+sudo apt -y remove needrestart apache2 nginx
 
 
 echo "@reboot root /opt/hiddify-central-config/install.sh >> /opt/hiddify-config/log/system/reboot.log 2>&1" > /etc/cron.d/hiddify_reinstall_on_reboot
--- status.sh
+++ status.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+cd $( dirname -- "$0"; )
 function main(){
-	systemctl status --no-pager caddy hiddify-panel|cat
+	systemctl status --no-pager **/*.service caddy|cat
+
+    for s in caddy **/*.service;do
+        s=${s##*/}
+        s=${s%%.*}
+        printf "%-30s %-30s \n" $s $(systemctl is-active $s)
+    done
 
     echo "---------------------Finished!------------------------"
 
