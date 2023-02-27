f45622285c78e2c2bac3401bb4a1c5681f946300
hiddify <hiddify@gmail.com>
2023-02-27 20:19:15 +0100

    fix: pkg: do not show release in release page !package

--- .github/release_message.sh
+++ .github/release_message.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 pip3 install gitchangelog pystache 2>&1 /dev/null
 
-previous_tag=$(git describe --tags $(git rev-list --tags --max-count=1))
+previous_release=$(curl --silent "https://api.github.com/repos/hiddify/hiddify-central-config/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")')
 current=$(cat VERSION)
-gitchangelog "${previous_tag}..v$current"
+gitchangelog "${previous_release}..v$current"
